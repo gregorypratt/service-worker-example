01d6787b365cfdab9f28c19a975cc2cf216fd730
Greg <greg@flashtalking.com>
2016-04-28 15:23:42 +0100

    Added message queue and post polling

--- index.js
+++ index.js
@@ -1,21 +1,31 @@
+var bodyParser = require('body-parser');
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+app.use(bodyParser.json());
 app.use(express.static('public'));
 
-io.on('connection', function (socket) {
-	console.log('a user connected');
-	socket.on('disconnect', function () {
-		console.log('user disconnected');
-	});
-	socket.on('chat message', function (msg) {
-		console.log('message: ' + msg);
-		io.emit('chat message', msg);
-	});
+app.get('/messages', function (req, res) {
+	res.status(200).send(['Hello there', 'this is another message, yay.']);
 });
 
+app.post('/message', function (req, res) {
+	if (!req.body || !req.body.msg) return res.sendStatus(400);
+	io.emit('chat message', req.body.msg);
+	res.sendStatus(200);
+});
+
+app.post('/messages', function (req, res) {
+	if (!req.body || !Array.isArray(req.body)) return res.sendStatus(400);
+	req.body.forEach(function (item) {
+		io.emit('chat message', item.msg);
+	});
+	res.sendStatus(200);
+});
+
+
 http.listen(3000, function () {
-	console.log('listening on *:3000');
+	console.log('Let\'s go');
 });
--- public/sw.js
+++ public/sw.js
@@ -1,17 +1,19 @@
 // The files we want to cache
 var urlsToCache = [
 	'/',
+	'/favicon.ico',
+	'/messages',
 	'/socket.io/socket.io.js'
 ];
 
-var CACHE_NAME = 'demo';
+var CACHE_NAME = 'demo-v1';
 
-self.addEventListener('install', function (event) {
+self.addEventListener('activate', function (event) {
 	// Perform install steps
 	event.waitUntil(
 		caches.open(CACHE_NAME)
 			.then(function (cache) {
-				console.log('Opened cache');
+				console.log('Service Worker:', 'Installed!');
 				return cache.addAll(urlsToCache);
 			})
 	);
@@ -21,42 +23,48 @@ self.addEventListener('install', function (event) {
  After a service worker is installed and the user navigates
  to a different page or refreshes, the service worker will
  begin to receive fetch events.
+
+ If they are cached then return them.
  */
 self.addEventListener('fetch', function (event) {
 	event.respondWith(
-		caches.match(event.request)
-			.then(function (response) {
-				// Cache hit - return response
-				if (response) {
-					return response;
-				}
-
-				// IMPORTANT: Clone the request. A request is a stream and
-				// can only be consumed once. Since we are consuming this
-				// once by cache and once by the browser for fetch, we need
-				// to clone the response
-				var fetchRequest = event.request.clone();
-
-				return fetch(fetchRequest).then(function (response) {
-						// Check if we received a valid response
-						if (!response || response.status !== 200 || response.type !== 'basic') {
-							return response;
-						}
-
-						// IMPORTANT: Clone the response. A response is a stream
-						// and because we want the browser to consume the response
-						// as well as the cache consuming the response, we need
-						// to clone it so we have 2 streams.
-						var responseToCache = response.clone();
-
-						caches.open(CACHE_NAME)
-							.then(function (cache) {
-								cache.put(event.request, responseToCache);
-							});
-
-						return response;
-					}
-				);
-			})
+		caches.match(event.request).then(function (response) {
+			return response || fetch(event.request);
+		})
 	);
 });
+
+/*
+ We want to utilise the Service Worker to de-couple our app
+ from the network. So it needs to be able to handles events
+ sent from the app.
+ */
+
+var messageQueue = [];
+
+self.addEventListener('message', function (event) {
+	var msg = event.data;
+	messageQueue.push(msg);
+	postMsgs();
+});
+
+function postMsgs() {
+	console.log(messageQueue);
+	if (messageQueue.length > 0) {
+		fetch('/messages', {
+			method: 'post',
+			headers: {
+				"Content-type": "application/json;charset=UTF-8"
+			},
+			body: JSON.stringify(messageQueue)
+		})
+			.then(function () {
+				messageQueue = [];
+			});
+	}
+}
+
+/*
+ For every 3 seconds check queue and send outstanding messages.
+ */
+setInterval(postMsgs, 3000);
